53b2e1f808edfabf6b8d554d245259aff8100e93
CrysPy-SAM <ssmishra9918@gmail.com>
2025-07-28 00:15:17 +0530

    for and for in  loop

--- 05_itretors/THREE.js
+++ 05_itretors/THREE.js
@@ -5,8 +5,18 @@ map.set('IN', "india" )
 map.set('USA', "United State of America")
 map.set('Fr',"France")
 
-console.log(map);
+// console.log(map);
 
 for (const key of map) {
-     console.log(key)
+     console.log(key, ':-', value);
+
 }
+
+const myObj = {
+     'game1' : 'NFS',
+     'game2' : 'spiderman'
+}
+//  for (const [key, value] of myObj){
+//      console.log(key,  ':-', value);
+
+//  }
