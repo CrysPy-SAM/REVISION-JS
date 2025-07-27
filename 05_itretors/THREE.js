0634520ab0ccca169f0284314b2e2c42d9299b0a
CrysPy-SAM <ssmishra9918@gmail.com>
2025-07-27 21:47:37 +0530

    loop

---
+++ 05_itretors/THREE.js
@@ -0,0 +1,12 @@
+// Maps
+
+const map = new Map()
+map.set('IN', "india" )
+map.set('USA', "United State of America")
+map.set('Fr',"France")
+
+console.log(map);
+
+for (const key of map) {
+     console.log(key)
+}
