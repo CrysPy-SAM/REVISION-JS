9d60bfd9d915d368f8758eac690fe3aa05e990d1
CrysPy-SAM <ssmishra9918@gmail.com>
2025-07-29 22:53:47 +0530

    for and for in  loop

---
+++ 05_itretors/six.js
@@ -0,0 +1,16 @@
+const coding =  ["ruby", "java","CPP","C","python","rust"]
+
+// const value = coding.forEach((item) =>{
+//     console.log(item);
+//     return item
+// })
+
+// console.log(value);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums.filter( (num) =>{ 
+    return num>4
+})
+console.log(newNums);
